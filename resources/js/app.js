0eecccc8b1fcc3d1e8cd9d0bab4aaab12eea057f
Daivid Quintero <daividqh1995@gmail.com>
2019-11-01 18:14:34 -0400

    Configuracion Vuetify Completa
    Configuracion de Plantilla Inicial (Interfaz) Empezada
    
    Configuracion de Interfaz de Login Iniciada.

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,7 @@
 //require('./bootstrap');
 
 import Vue     from 'vue';
-import vuetify from 'vuetify';
+import vuetify from './plugins/vuetify';
 import router  from './routers';
 import store   from './stores';
 
